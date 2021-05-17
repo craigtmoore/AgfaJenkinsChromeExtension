c6373ab0b2a5a1d2589b8219b42eb0453bcd25d5
Craig Moore <craig.moore@agfa.com>
2021-05-17 00:16:47 -0400

    Update popup: enable list items so they can be clicked and fix html option

--- Extension/popup.js
+++ Extension/popup.js
@@ -11,41 +11,25 @@ function onWindowLoad() {
     commands.map(showKeybinding);
   });
 
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+  chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
     var tabId = tabs[0].id;
+    var url = tabs[0].url
     chrome.tabs.sendMessage(tabId, {action: 'get page details'}, function(response) {
-      if ( ! response ) {
-        var lis = document.getElementsByTagName('li');
-        for ( var i = 0; i < lis.length; i++ ) {
-          lis[i].style.cursor = 'not-allowed';
-        }
-
-        return;
-      }
-
       var titleItem = document.getElementById('copy-jira');
       titleItem.onclick = function(event) {
-        snagTitle(response);
+        snagJiraFormat(url, response);
         window.close();
       }
 
       var urlItem = document.getElementById('copy-markdown');
       urlItem.onclick = function(event) {
-        snagLink(response);
+        snagMarkdownFormat(url, response);
         window.close();
       }
 
       var htmlItem = document.getElementById('copy-html');
       htmlItem.onclick = function(event) {
-        snagHtmlLink(response);
-        window.close();
-      }
-
-      // We can't set window.location.href from within a popup's code,
-      // so delegate to the code we've injected into the host page.
-      var emailItem = document.getElementById('send-email');
-      emailItem.onclick = function(event) {
-        chrome.tabs.sendMessage(tabId, {action: 'send e-mail'});
+        snagHtmlFormat(url, response);
         window.close();
       }
     });
